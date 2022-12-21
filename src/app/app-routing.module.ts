e35ced7997598ae8e02a9b5b79b30bdd6738971b
VikasSahu1998 <sahuvikas1998@gmail.com>
2022-12-21 12:10:59 +0530

    comming soon added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoriesComponent } from './categories/categories.component';
+import { ComingSoonComponent } from './coming-soon/coming-soon.component';
 import { HomeComponent } from './home/home.component';
 import { AirTicketComponent } from './SideNavBar/air-ticket/air-ticket.component';
 import { OnDemandServicesFilComponent } from './SideNavBar/on-demand-services-fil/on-demand-services-fil.component';
@@ -12,6 +13,7 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 const routes: Routes = [
   { path: 'Home', component: HomeComponent },
   { path: 'Shopping', component: ShoppingComponent },
+  { path: 'ComingSoon', component: ComingSoonComponent },
   { path: 'On-Demand-Services_fil', component: OnDemandServicesFilComponent },
   { path: 'AirTicket', component: AirTicketComponent },
   { path: 'Categories', component: CategoriesComponent },
